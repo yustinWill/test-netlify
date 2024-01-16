60ca163b04a1d5bd0f88ce3d3ce098cb755f20ab
yustinWill <yustin.will@gmail.com>
2024-01-17 00:14:34 +0700

    update image

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,22 @@
+import IMAGE from '../assets/images/avatar.jpg'
+
 export default function Index() {
+  const imageSource = IMAGE
+
   return (
-    <p id="zero-state">
-      This is a demo for React Router.
-      <br />
-      Check out{" "}
-      <a href="https://reactrouter.com">
-        the docs at reactrouter.com
-      </a>
-      .
-    </p>
+    <>
+      <p id="zero-state">
+        This is a demo for React Router.
+        <br />
+        Check out{" "}
+        <a href="https://reactrouter.com">
+          the docs at reactrouter.com
+        </a>
+        .
+      </p>
+      <img
+        src={imageSource}
+      />
+    </>
   );
 }
